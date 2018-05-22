5865f555f6cf578541536ea7e38994fa186981bf
Max King <maxdavidking@gmail.com>
2018-05-22 09:53:26 -0500

    Add basic outline of tsv writer to write xml content

--- fund_holdings_parser.py
+++ fund_holdings_parser.py
@@ -1,3 +1,7 @@
 from lxml import etree
 import requests
 import csv
+
+#Open new TSV file to save data in tab separated format
+ticker_data = open('tmp/ticker.tsv', 'w')
+csvwriter = csv.writer(ticker_data, delimiter='\t')
