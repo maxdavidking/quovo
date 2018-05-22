f3004c17afa21bde05989798dd2b2a45d283406a
Max King <maxdavidking@gmail.com>
2018-05-22 09:53:54 -0500

    Add hardcoded path to XML file

--- fund_holdings_parser.py
+++ fund_holdings_parser.py
@@ -5,3 +5,6 @@ import csv
 #Open new TSV file to save data in tab separated format
 ticker_data = open('tmp/ticker.tsv', 'w')
 csvwriter = csv.writer(ticker_data, delimiter='\t')
+
+#Hard code link directly to XML for now
+link = "https://www.sec.gov/Archives/edgar/data/1166559/000110465918033472/a18-13444_1informationtable.xml"
