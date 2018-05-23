cd0c0252d1d5e32a76b6a0f2a159bd668cfc8de6
Max King <maxdavidking@gmail.com>
2018-05-23 12:11:33 -0500

    Transform xml lookup steps into functions called in main

--- fund_holdings_parser.py
+++ fund_holdings_parser.py
@@ -8,33 +8,52 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
 def main():
+    search_results = cik_lookup()
+    print search_results
+    form_results = form_lookup(search_results)
+    print form_results
+    filing_details = get_xml_url(form_results)
+    print filing_details
+
+def cik_lookup():
     #Include getopts options
-    #Have error message
+    #Have error handling
     driver = webdriver.Firefox()
     driver.get("https://www.sec.gov/edgar/searchedgar/companysearch.html")
     element = driver.find_element_by_id("cik")
+    #Pass CIK from command line arguments to search box
     element.send_keys(sys.argv[1])
     element.submit()
+    #Call wait to ensure that new page loads
+    wait_for_load(driver, 'documentsbutton', 'Documents')
+    return driver.current_url
+
+def form_lookup(url):
+    #Have error handling
+    driver = webdriver.Firefox()
+    driver.get(url)
+    element = driver.find_element_by_xpath("//*[contains(text(), '13F-HR')]/following::td")
+    element.click()
+    #Call wait to ensure that new page loads
+    wait_for_load(driver, 'formName', 'Form 13F-HR')
+    return driver.current_url
+
+def get_xml_url(url):
+    #Have error handling
+    driver = webdriver.Firefox()
+    driver.get(url)
+    element = driver.find_element_by_partial_link_text("able.xml")
+    return element.get_attribute("href")
+
+#Selenium will look for an element on a page without waiting for the new page
+#to load. This function ensures the new page loads
+def wait_for_load(driver, div_id, text):
     wait(driver, 3).until(
     expected_conditions.text_to_be_present_in_element(
-        (By.ID, 'documentsbutton'),
-        'Documents'
+        (By.ID, div_id),
+        text
         )
     )
-    #Return this URL and pass to new function that looks for 'documents'?
-    element2 = driver.find_element_by_xpath("//*[contains(text(), '13F-HR')]/following::td")
-    element2.click()
-    wait(driver, 3).until(
-    expected_conditions.text_to_be_present_in_element(
-        (By.ID, 'formName'),
-        'Form 13F-HR'
-        )
-    )
-    element3 = driver.find_element_by_partial_link_text("able.xml")
-    #This is the URL
-    return element3.get_attribute("href")
-
-
 
 #Hard code link directly to XML for now
 link = "https://www.sec.gov/Archives/edgar/data/1166559/000110465918033472/a18-13444_1informationtable.xml"
@@ -82,9 +101,9 @@ for fund in funds:
 #Temporary strip of all but first x elements - also strip out namespace from header
 reduced_headers = headers[:12]
 for x in reduced_headers:
-    print type(x)
+    #print type(x)
     x_split = x.split('}', 1)[-1]
-    print x_split
+    #print x_split
 
 #Create TSV name - need to add pass through from ARGV
 tsvFilename = "tmp/ticker.tsv"
@@ -96,7 +115,7 @@ tsv.write(col_names)
 tsv.write('\n')
 for values in funds_text:
     string_values = '\t'.join(values)
-    print string_values
+    #print string_values
     #write to tsv
     tsv.write(string_values)
     #Move to new line
